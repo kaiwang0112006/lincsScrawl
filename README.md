c9bcc7108d1b12c4b73f5c3c8a7605404afebba2
kaiwang <wangkai0112006@163.com>
2016-02-29 21:44:27 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# lincsScrawl
+This is a project that is built to store scripts for Tuotuo to scrawle [lincs](http://lincs.hms.harvard.edu/kinomescan/)
+
+
+How to run
+---------
+Please found a sample run I did and useful note at [Note](https://github.com/kaiwang0112006/lincsScrawl/blob/master/example/note)
---
+++ src/scrawlMain.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import argparse
+import csv
+from subprocess import Popen, PIPE
+
+##########################################
+## Options and defaults
+##########################################
+def getOptions():
+    parser = argparse.ArgumentParser(description='python *.py [option]"')
+    requiredgroup = parser.add_argument_group('required arguments')
+    requiredgroup.add_argument('--in', dest='input', help='input file', default='', required=True)
+
+    args = parser.parse_args()
+
+    return args
+
+##########################################
+## Master function
+##########################################           
+def main():
+    options = getOptions()
+    
+    data = []
+    
+    with open(options.input) as fin:
+        fcsv = csv.reader(fin)
+        for eachline in fcsv:
+            if fcsv.line_num == 1:
+                continue
+            link = eachline[-1]
+            id = eachline[-1].split('/')[-2]
+            id = '20211'
+            cmd = "curl -H \"Accept: text/csv\" http://lincs.hms.harvard.edu/db/api/v1/datasetdata/%s/ > %s.csv" % (id,id)
+            dataset = "%s.csv" % id
+            
+            p = Popen(cmd, stdout=PIPE, stderr=PIPE,shell=True)
+            stdout, stderr = p.communicate()
+            
+            try:
+                with open(dataset) as fdata:
+                    datacsv = csv.reader(fdata,quotechar='"')
+                    for eachline in datacsv:
+                        if datacsv.line_num == 1:
+                            title = eachline
+                            continue
+                        if eachline[5] == '200122':
+                            data.append(eachline)
+            except:
+                print 'error read file %s' % dataset
+                
+    with open('result.csv', 'wb') as csvout:
+        csvwriter = csv.writer(csvout, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csvwriter.writerow(title)
+        csvwriter.writerows(data)
+                    
+            
+            
+            
+            
+            
+        
+    
+if __name__ == "__main__":
+    main()
